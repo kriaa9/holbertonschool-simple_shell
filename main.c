8f515e66cc3b75a77a2093cb9483b75ba14b7c61
kriaa9 <11596@holbertonstudents.com>
2025-12-22 21:16:47 +0100

    Task 1: Betty compliant main and header

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * main - entry point for the simple shell
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	(void)ac;
+	(void)av;
+
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* Buffer sizes */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* Prototypes will go here later */
+
+#endif /* SHELL_H */
